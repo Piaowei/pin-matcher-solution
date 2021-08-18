01972d5903b50a6ac8a4b1f44e878d00baf72266
Piaowei <msptonmoy@gmail.com>
2021-08-19 01:07:00 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+function getPin() {
+	const pin = Math.round(Math.random() * 10000)
+	const pinString = pin + "";
+	if (pinString.length == 4) {
+		return pin;
+	}
+	else {
+		// console.log("got 3 digit and calling again", pin);
+		return getPin();
+	}
+}
+
+function generatePin() {
+	const pin = getPin();
+	document.getElementById("display-pin").value = pin;
+
+}
+
+// calculator
+
+document.getElementById("key-pad").addEventListener("click", function (event) {
+	const number = event.target.innerText;
+	const calcInput = document.getElementById("typed-numbers");
+	if (isNaN(number)) {
+		if (number == "C") {
+			calcInput.value = "";
+		}
+	}
+	else {
+
+		const previousNumber = calcInput.value;
+		const newNumber = event.target.innerText;
+		calcInput.value = previousNumber + newNumber;
+		// console.log("this is type of -", typeof number, number.length);
+
+	}
+
+})
+
+function varifyPin() {
+	debugger;
+	const pin = document.getElementById('display-pin').value;
+	const typedNumbers = document.getElementById("typed-numbers").value
+	const successMessege = document.getElementById("notify-success");
+	const failError = document.getElementById("notify-failed");
+
+	if (pin == typedNumbers) {
+
+		successMessege.style.display = 'block';
+		failError.style.display = 'none';
+	}
+	else {
+
+		failError.style.display = 'block';
+		successMessege.style.display = 'none';
+	}
+}
